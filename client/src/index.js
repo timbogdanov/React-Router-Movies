73e1928ae0a4de65a63f2dbd349192a500199e8c
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-06-15 15:50:03 -0700

    complete assignment

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,4 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, document.getElementById('root'));
+  <Router><App /></Router>, document.getElementById('root'));
